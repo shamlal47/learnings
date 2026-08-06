38d7ddea4da269c67fae29c03da8824d55a4d342
shamlal47 <Shamlalthegreat@gmail.com>
2026-08-06 18:34:00 +0530

    ditriangle pattern done

--- 8-14Patterns.java
+++ 8-14Patterns.java
@@ -3,6 +3,7 @@ class Patterns {
         Patterns p = new Patterns();
         p.rev_Pyramid(5);
         p.diamond(5);
+        p.ditriangle(5);
     }
 
     public void rev_Pyramid(int n) {
@@ -49,6 +50,23 @@ class Patterns {
             System.out.println();
 
         }
+    }
+
+    public void ditriangle(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 
 }
