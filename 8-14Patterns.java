3aa8815047f7c8e21c2dcfd09bf8fea010356904
shamlal47 <Shamlalthegreat@gmail.com>
2026-08-06 18:27:21 +0530

    diamond pattern done

--- 8-14Patterns.java
+++ 8-14Patterns.java
@@ -2,9 +2,40 @@ class Patterns {
     public static void main(String[] args) {
         Patterns p = new Patterns();
         p.rev_Pyramid(5);
+        p.diamond(5);
     }
 
     public void rev_Pyramid(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public void diamond(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
