69525f2ae98e58bb4fb8b69cdbe7e3c629b949bf
shamlal47 <Shamlalthegreat@gmail.com>
2026-08-07 17:39:22 +0530

    abc_tri2 completed

--- 15-22Patterns.java
+++ 15-22Patterns.java
@@ -2,12 +2,23 @@ class Patterns {
     public static void main(String[] args) {
         Patterns p = new Patterns();
         p.rev_abc_tri(5);
+        p.abc_tri2(5);
     }
 
     void rev_abc_tri(int n) {
         for (int i = n; i >= 1; i--) {
             for (char ch = (char) ('A' + i - 1); ch >= 'A'; ch--) {
                 System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+
+    void abc_tri2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                char ch = (char) ('A' + i);
+                System.out.print(ch + " ");
             }
             System.out.println();
         }
