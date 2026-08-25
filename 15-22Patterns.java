1060e9d4e64fd1cdf2347e8c3746914bb7ff3d61
shamlal47 <Shamlalthegreat@gmail.com>
2026-08-25 19:33:33 +0530

    diamond space added

--- 15-22Patterns.java
+++ 15-22Patterns.java
@@ -5,6 +5,7 @@ class Patterns {
         p.abc_tri2(5);
         p.abc_pyramid2(5);
         p.abc_rev(5);
+        p.diend_space(5);
     }
 
     void rev_abc_tri(int n) {
@@ -60,5 +61,51 @@ class Patterns {
             System.out.println();
         }
     }
+
+    void diend_space(int n){
+
+        int space = 0;
+
+        for(int i = 0;i<n;i++){
+
+            for(int j = 1;j <= n-i;j++){
+                System.out.print("*");
+            }
+
+            for(int j = 0; j < space;j++){
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("*");
+            }
+
+            space = space + 2;
+            System.out.println();
+        }
+
+        space = 2 * n - 2;
+
+        for(int i = 1;i<=n;i++){
+
+            for(int j = 1;j <= i;j++){
+                System.out.print("*");
+            }
+
+            for(int j = 0; j < space;j++){
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            space = space - 2;
+            System.out.println();
+        }
+
+
+    }
+
 
 }
